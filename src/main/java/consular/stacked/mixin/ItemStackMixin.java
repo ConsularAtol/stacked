2c56f46a32b6b6dac8678a859b9305a31bb2cf64
ConsularAtol <dennis21806@gmail.com>
2025-08-13 19:59:54 -0400

    No more bundles

--- src/main/java/consular/stacked/mixin/ItemStackMixin.java
+++ src/main/java/consular/stacked/mixin/ItemStackMixin.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemStack;
 import net.minecraft.component.ComponentHolder;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.registry.tag.ItemTags;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,7 +19,7 @@ public abstract class ItemStackMixin implements ComponentHolder, FabricItemStack
 	@Inject(at = @At("TAIL"), method = "getMaxCount")
 	private int init(CallbackInfoReturnable<?> info) {
 		ItemStack stack = (ItemStack)(Object)this;
-		if(stack.getMaxDamage() == 0 && !stack.hasEnchantments() && !stack.isOf(Items.ENCHANTED_BOOK)){
+		if(stack.getMaxDamage() == 0 && !stack.hasEnchantments() && !stack.isOf(Items.ENCHANTED_BOOK) && stack.isIn(ItemTags.BUNDLES)){
 			return ConfigManager.getMaxStackSize();
 		}else{
 			return 1;
